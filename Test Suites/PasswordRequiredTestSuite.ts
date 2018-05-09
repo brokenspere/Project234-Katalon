303c2abd236b9a90271081804b4d2e14302e3af5
MSI-GE72VR <nattapat013@outlook.com>
2018-05-09 16:41:30 +0700

    add required password test include test data

---
+++ Test Suites/PasswordRequiredTestSuite.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PasswordRequiredTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-09T16:36:02</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c82f798a-9289-49db-8d81-8057d248bd1b</testSuiteGuid>
+   <testCaseLink>
+      <guid>ee011d4f-583e-4c35-bcb9-6f3b34c577da</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestRequirePassword</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f816c995-6c83-4ab3-8eab-0622457f8312</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/PasswordRequired</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>f816c995-6c83-4ab3-8eab-0622457f8312</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>1824af25-ca2f-447d-9ac1-7c3a8e94c99b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f816c995-6c83-4ab3-8eab-0622457f8312</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>f4a2d960-ba56-4686-925b-6d1ab69ec0e1</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
